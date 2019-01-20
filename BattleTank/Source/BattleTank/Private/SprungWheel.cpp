b32ab3fb857be2c7867a8a17ddb6ec12f10d8d60
Dana Costa <24557170+dcosta2@users.noreply.github.com>
2019-01-20 14:50:02 -0500

    Working on suspension for tank

---
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SprungWheel.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+
+// Sets default values
+ASprungWheel::ASprungWheel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	PhysicsConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("PhysicsConstraint"));
+	SetRootComponent(PhysicsConstraint);
+
+	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
+	Wheel->SetupAttachment(PhysicsConstraint);
+
+	
+}
+
+// Called when the game starts or when spawned
+void ASprungWheel::BeginPlay()
+{
+	Super::BeginPlay();
+	SetupConstraint();
+}
+
+void ASprungWheel::SetupConstraint() {
+	if (GetAttachParentActor())
+	{
+
+		UPrimitiveComponent* BodyRoot = Cast<UPrimitiveComponent>(GetAttachParentActor()->GetRootComponent());
+		PhysicsConstraint->SetConstrainedComponents(
+			BodyRoot,
+			NAME_None,
+			Cast<UPrimitiveComponent>(Wheel),
+			NAME_None
+		);
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Null"));
+	}
+}
+// Called every frame
+void ASprungWheel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
